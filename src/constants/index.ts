6221d16188940e3c71378b546800ec89179365f7
Waldemar Glaz <waldekglaz@gmail.com>
2025-10-17 10:33:06 +0200

    FeaturedProjects section

---
+++ src/constants/index.ts
@@ -0,0 +1,42 @@
+const featuredProjects = [
+  {
+    title: "Web Applications",
+    description:
+      "Building dynamic and interactive web applications using modern frameworks and technologies.",
+    image: "/works/fem-entert.png",
+    link: "/projects/#web-applications",
+  },
+  {
+    title: "Landing Pages",
+    description:
+      "Creating responsive and engaging landing pages that convert visitors into customers.",
+    image: "/works/designo.png",
+    link: "/projects/#landing-pages",
+  },
+  {
+    title: "E-commerce Sites",
+    description:
+      "Developing user-friendly e-commerce platforms that enhance the shopping experience.",
+    image:
+      "https://www.techcoit.com/wp-content/uploads/2025/04/ab04fd36787cb2b3fe8e9d798c4f6e8a.webp",
+    link: "/projects/#ecommerce-sites",
+  },
+  {
+    title: "HTML Email Templates",
+    description:
+      "Designing and coding responsive HTML email templates for effective email marketing campaigns.",
+    image:
+      "https://moosend.com/wp-content/uploads/2023/05/beefree-html-email-template.png",
+    link: "/projects/#email",
+  },
+  {
+    title: "Animated HTML Banners",
+    description:
+      "Creating eye-catching animated HTML banners to boost online advertising efforts.",
+    image:
+      "https://img.freepik.com/free-vector/web-design-job-facebook-cover-template_23-2150993799.jpg?semt=ais_hybrid&w=740&q=80",
+    link: "/projects/#banners",
+  },
+];
+
+export default featuredProjects;
